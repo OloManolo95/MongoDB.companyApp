270faedb8e0f0cce52acf156fda22cabc64ca2f8
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-10-09 19:38:04 +0200

    add controler files and separate functions from the routes

---
+++ controllers/departments.controller.js
@@ -0,0 +1,90 @@
+const Department = require('../models/department.model');
+
+exports.getAll = async (req, res) => {
+    try {
+      res.json(await Department.find());
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+  };
+  
+  exports.getRandom = async (req, res) => {
+  
+    try {
+      const count = await Department.countDocuments();
+      const rand = Math.floor(Math.random() * count);
+      const dep = await Department.findOne().skip(rand);
+      if(!dep) res.status(404).json({ message: 'Not found' });
+      else res.json(dep);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+  };
+  
+  exports.getByid = async (req, res) => {
+  
+    try {
+      const dep = await Department.findById(req.params.id);
+      if(!dep) res.status(404).json({ message: 'Not found' });
+      else res.json(dep);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+  };
+  
+  exports.addNew = async (req, res) => {
+  
+    try {
+  
+      const { name } = req.body;
+      const newDepartment = new Department({ name: name });
+      await newDepartment.save();
+      res.json({ message: 'OK' });
+  
+    } catch(err) {
+      res.status(500).json({ message: err });
+    }
+  
+  };
+  
+  exports.modifyById = async (req, res) => {
+    const { name } = req.body;
+  
+    try {
+      const dep = await Department.findById(req.params.id);
+  
+      if (!dep) {
+        res.status(404).json({ message: 'Not found...' });
+        return;
+      }
+  
+      dep.name = name;
+      await dep.save();
+  
+      res.json({ message: 'OK', updatedDepartment: dep });
+    } catch (err) {
+      res.status(500).json({ message: err });
+    }
+  };
+  
+  exports.deleteById = async (req, res) => {
+    try {
+      const dep = await Department.findById(req.params.id);
+  
+      if (!dep) {
+        res.status(404).json({ message: 'Not found...' });
+        return;
+      }
+  
+      const deletedDep = await Department.findOneAndDelete({ _id: req.params.id });
+  
+      res.json({ message: 'OK', deletedDepartment: deletedDep });
+    } catch (err) {
+      res.status(500).json({ message: err });
+    }
+  };
